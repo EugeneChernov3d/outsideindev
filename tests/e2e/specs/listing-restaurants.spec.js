0dbaca5708e2def1abe59be247f02dd7927ed0c0
zhenek <zhenek>
2020-07-08 00:23:11 +0300

    Specify app should list restaurants

---
+++ tests/e2e/specs/listing-restaurants.spec.js
@@ -0,0 +1,21 @@
+describe('Listing Restaurants', () => {
+  it('shows restaurants from the server', () => {
+    const sushiPlace = 'Sushi Place';
+    const pizzaPlace = 'Pizza Place';
+
+    cy.server({force404: true});
+
+    cy.route({
+      method: 'GET',
+      url: 'https://api.outsidein.dev/1Rb1oOqz6WWcXbtyMMeEqOG2FeUcsBIS/restaurants',
+      response: [
+        {id: 1, name: sushiPlace},
+        {id: 2, name: pizzaPlace},
+      ],
+    });
+
+    cy.visit('/');
+    cy.contains(sushiPlace);
+    cy.contains(pizzaPlace);
+  });
+});
